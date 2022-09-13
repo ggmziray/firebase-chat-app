b6bbbdec7490d119f53b6290c1db722140215f18
Gillead-Gaari Mziray <66039579+ggmziray@users.noreply.github.com>
2022-09-13 17:59:59 +0100

    Add files via upload

---
+++ firebase-chat.js
@@ -0,0 +1,140 @@
+(function () {
+  // TODO: replace this with your own firebase config object after creating app in your firebase console
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxTC6ic_Zir_rDcfo4HFd5XU9OnPBeN3E",
+    authDomain: "mit-courso-99.firebaseapp.com",
+    databaseURL: "https://mit-courso-99-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mit-courso-99",
+    storageBucket: "mit-courso-99.appspot.com",
+    messagingSenderId: "868907885042",
+    appId: "1:868907885042:web:c74f75f623be5e85c9cb6b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // handle on firebase db
+  const db = firebase.database();
+
+  // get elements
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const login = document.getElementById('login');
+  const signup = document.getElementById('signup');
+  const logout = document.getElementById('logout');
+  const message = document.getElementById('message');
+  const write = document.getElementById('write');
+  const read = document.getElementById('read');
+  const status = document.getElementById('status');
+  const userNameDisplay = document.getElementById('name-display'); // element that can show the current user's email
+  const chat = document.getElementById('chat-box');
+  let currentUserEmail = 'lolo@lucy.com'; // variable to store the current user's email
+
+  // write
+  write.addEventListener('click', (e) => {
+    const messages = db.ref('messages');
+
+    // simple id - ok for example, do not use in production
+    const id = new Date().getTime();
+
+    // TODO: Add the value of currentUserEmail when writing to the database under the field name of "sender"
+    messages
+      .child(id)
+      .set({ message: message.value, sender: currentUserEmail })
+      .then(function () {
+        console.log('Wrote to DB!');
+      });
+  });
+
+  // read
+  read.addEventListener('click', (e) => {
+    handleRead();
+  });
+
+  // TODO: use this provided messagesRef to listen for updates and update the chat div on any update, not just when the 'Update Chat' button is clicked
+  const messagesRef = db.ref('messages');
+  messagesRef.on('value', (snapshot) => {
+    handleRead();
+  });
+
+  // This function triggered on click parses the contents of the messages array to show them in the chat window
+  function handleRead() {
+    status.innerHTML = '';
+    chat.innerHTML = '';
+    const messages = db.ref('messages');
+
+    messages.once('value').then(function (dataSnapshot) {
+      var data = dataSnapshot.val();
+      if (data) {
+        var keys = Object.keys(data);
+
+        keys.forEach(function (key) {
+          console.log(data[key]);
+          chat.innerHTML +=
+            (data[key]['sender'] || '') +
+            '   :   ' +
+            data[key].message +
+            '<br><br>';
+        });
+      }
+    });
+  }
+
+  // TODO: in this function you should set the userNameDisplay.innerHTML to the passed in userEmail as well as updating the currentUserEmail variable to that same value
+  function updateCurrentUser(userEmail) {
+    userNameDisplay.innerHTML = userEmail;
+    currentUserEmail = userEmail;
+  }
+
+  // login
+  login.addEventListener('click', (e) => {
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.then((resp) => {
+      console.log('User Login Response: ', resp);
+      logout.style.display = 'inline';
+      login.style.display = 'none';
+      signup.style.display = 'none';
+      write.style.display = 'inline';
+      updateCurrentUser(resp.user.email);
+    });
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // signup
+  signup.addEventListener('click', (e) => {
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.then((resp) => {
+      console.log('User Signup + Login Response: ', resp);
+      logout.style.display = 'inline';
+      login.style.display = 'none';
+      signup.style.display = 'none';
+      write.style.display = 'inline';
+      updateCurrentUser(resp.user.email);
+    });
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // logout
+  logout.addEventListener('click', (e) => {
+    firebase
+      .auth()
+      .signOut()
+      .then((resp) => {
+        console.log('Logout Response: ', resp);
+        logout.style.display = 'none';
+        login.style.display = 'inline';
+        signup.style.display = 'inline';
+        write.style.display = 'none';
+        updateCurrentUser('');
+      })
+      .catch((e) => console.warn(e.message));
+  });
+})();
